d30be1e11a616aae83218f83a69be79e6bb6709f
jotavital <joaopedrovitaldoctum@gmail.com>
2022-01-10 23:25:11 -0300

    feat: :art: learning 'bout components, variables inside JSX etc...

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+
+    const nome = "Vital";
+    const imagem = "https://via.placeholder.com/250";
+
+    function sum(val1, val2) {
+        return val1 + val2;
+    }
+
     return (
         <div className="App">
-            <h1>Olá React</h1>
-            <p>Meu primeiro app react</p>
+            <div>
+                <h2>Meu nome é {nome}</h2>
+                <p>2 + 2 são exatamente {sum(2, 2)}</p>
+            </div>
+            <div>
+                <img src={imagem} alt={imagem} />
+            </div>
+            <hr />
+            <HelloWorld />
         </div>
     );
 }
---
+++ meu-projeto/src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    return ( 
+        <div>
+            <p>Isso é um componente contendo uma frase</p>
+        </div>
+    );
+}
+
+export default Frase;
