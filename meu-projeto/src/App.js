fddbfd859e7df7e85c3a0978395e5e34d72f3a86
jotavital <joaopedrovitaldoctum@gmail.com>
2022-01-10 22:42:18 -0300

    feat: :art: primeiro commit

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Olá React</h1>
+            <p>Meu primeiro app react</p>
+        </div>
+    );
+}
+
+export default App;
