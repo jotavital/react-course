23d864a1ccfd80191eb797039fc8f1b4e384f6ab
jotavital <joaopedrovitaldoctum@gmail.com>
2022-01-20 11:25:39 -0300

    feat: :art: react icons

--- meu-projeto/src/components/Footer.js
+++ meu-projeto/src/components/Footer.js
@@ -1,7 +1,14 @@
+import { FaInstagram, FaFacebook } from 'react-icons/fa';
+
 function Footer() {
     return (
         <div>
             <footer>
+                <p>
+                    <FaInstagram />
+                    <FaFacebook />
+                    Segue nois no instagrando
+                </p>
                 <p>All rights Reserved buddy</p>
             </footer>
         </div>
